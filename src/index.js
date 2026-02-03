f2991d33fbe31c2265888734018144f045df43c9
Audun Kronstad <141585731+boolsen@users.noreply.github.com>
2026-02-03 21:53:21 +0100

    Adding logic for placing ships
    Checks that there aren't too many surrounding ships,
    and that the coordinates are in the grid
    
    Also checks that the cell is in the right orientation
    to the existing ship

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import "./style.css";
 
 class Ship {
-    constructor(length) {
-        this.length = length;
+    constructor() {
+        this.length = 0;
         this.hits = 0;
         this.sunk = false;
+        this.cells = [];
     }
 
     hit(){
@@ -16,6 +17,10 @@ class Ship {
         if (this.hits >= this.length){
             this.sunk = true;
         }
+    }
+
+    addCell(x,y) {
+        this.cells.push([x,y]);
     }
 }
 
@@ -37,23 +42,59 @@ class GameBoard {
     }
 
     placeShipInCell(x,y) {
+        const placeShipCheck = this.canPlaceShip(x,y);
+        if (!placeShipCheck.canPlaceShip || placeShipCheck.shipsPlacedAroundCell.length > 1) {
+            console.log("Can't place ship");
+            return;
+        }
+
+        if (placeShipCheck.shipsPlacedAroundCell.length == 1 && placeShipCheck.shipsPlacedAroundCell[0].length > 1) {
+            if (this.checkShipPlacement(x, y, placeShipCheck.shipsPlacedAroundCell[0])) {
+                console.log("Can't place ship, wrong orientation");
+                return;
+            }
+        }
 
         this.grid[y][x].placeShip();
     }
 
-    neighBourCellIsShip(x,y) {
-        let shipCount = 0;
-        for (let i = -1; i < 2; i += 2) {
+    checkShipPlacement(x,y, ship) {
+        let orientation = null;
+
+        const uniqueXCoords = new Set(ship.cells.map(x => x[0]));
+        if (uniqueXCoords.lenght = 1 && x == uniqueXCoords[0]) {
+            return true;
+        } 
+        const uniqueYCoords = new Set(ship.cells.map(y => y[1]));
+         if (uniqueYCoords.lenght = 1 && x == uniqueYCoords[0]) {
+            return true;
+        } 
+        return false;        
+    }
+
+    canPlaceShip(x,y) {
+        const shipsPlacedAroundCell = [];
+        if (x < 0 || x >= this.gridSize || y < 0 || y >= this.gridSize || this.grid[x][y].ship) {
+            return {canPlaceShip: false, shipsPlacedAroundCell: shipsPlacedAroundCell};
+        }
+
+        for (let i = -1; i < 2; i++) {
             if (x + i < this.gridSize - 1) {
                 if (this.grid[x + i][y].ship !== null) {
-                    shipCount++;
+                    shipsPlacedAroundCell.push([x + i, y]);
                 }                
             }
             if (y + i < this.gridSize - 1) {
                 if (this.grid[x][y + i].ship !== null) {
-                    shipCount++;
+                    shipsPlacedAroundCell.push([x,y + i]);
                 }                
             }
+        }
+
+        if (shipsPlacedAroundCell > 1) {
+            return {canPlaceShip: false, shipsPlacedAroundCell: shipsPlacedAroundCell};
+        } else {
+            return {canPlaceShip: true, shipsPlacedAroundCell: shipsPlacedAroundCell};
         }
     }
 }
@@ -64,9 +105,12 @@ class Cell {
         this.xCoord = x;
         this.yCoord = y;
         this.hit = false;
+        this.element = null;
     }
 
-    placeShip(ship) {
+    placeShip(ship, x, y) {
         this.ship = ship;
+        ship.cells.addCell(x,y);
+        ship.length++;
     }
 }
